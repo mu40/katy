7082bf2dcc35b51af3b9a7a123233fd1db3dfca0
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2024-11-15 11:21:14 -0500

    Document filter tests.

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -6,6 +6,7 @@ import kathryn as kt
 
 
 def test_gaussian_kernel_properties():
+    """Test kernel width control, normalization, dimensionality, data type."""
     width = 13
     k = kt.filter.gaussian_kernel(fwhm=8, width=13)
     assert k.numel() == width
@@ -15,14 +16,17 @@ def test_gaussian_kernel_properties():
 
 
 def test_gaussian_kernel_default_width_odd():
+    """Test if the default kernel size is odd."""
     assert kt.filter.gaussian_kernel(fwhm=8).numel() % 2 == 1
 
 
 def test_blur_fwhm_zero():
+    """Test if blurring with zero FWHM has no effect."""
     x = torch.randn(3, 4, 5)
     assert kt.filter.blur(x, fwhm=0).allclose(x)
 
 
 def test_blur_default_dtype():
+    """Test the default output data type."""
     x = torch.randint(10, size=(5,))
     assert kt.filter.blur(x, fwhm=2).dtype == torch.get_default_dtype()
