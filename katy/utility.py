6c99e90ddbfef17d297caf64e93c93e45bf18da2
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2024-12-11 18:56:48 -0500

    Resize tensors symmetrically.

--- katy/utility.py
+++ katy/utility.py
@@ -29,3 +29,40 @@ def chance(prob, size=1, device=None, generator=None):
 
     size = torch.as_tensor(size).ravel()
     return torch.rand(*size, device=device, generator=generator) < prob
+
+
+def resize(x, size, fill=0):
+    """Symmetrically crop or pad an N-dimensional tensor to a new size.
+
+    Parameters
+    ----------
+    x : (B, C, ...) torch.Tensor
+        Tensor of `N`-element spatial shape.
+    size : int or sequence of int
+        New spatial shape. Pass 1 or N values.
+    fill : float, optional
+        Fill value for padding.
+
+    Returns
+    -------
+    (B, C, *size) torch.Tensor
+        Resized tensor.
+
+    """
+    # Dimensions. Keep batch and channel dimensions.
+    ndim = x.ndim - 2
+    size_old = torch.as_tensor(x.shape)
+    size_new = torch.as_tensor(size).ravel().expand(ndim)
+    size_new = torch.cat((size_old[:2], size_new))
+
+    # Indexing. Non-zero starting indices into the input are equivalent to
+    # cropping, and those into the output are equivalent to padding.
+    overlap = torch.minimum(size_old, size_new)
+    a_old = size_old.sub(size_new).clamp(min=0) // 2
+    a_new = size_new.sub(size_old).clamp(min=0) // 2
+    ind_old = tuple(map(slice, a_old, a_old + overlap))
+    ind_new = tuple(map(slice, a_new, a_new + overlap))
+
+    out = torch.full(size_new.tolist(), fill, device=x.device)
+    out[ind_new] = x[ind_old]
+    return out
--- tests/test_utility.py
+++ tests/test_utility.py
@@ -43,3 +43,59 @@ def test_chance_illegal_values():
 
     with pytest.raises(ValueError):
         kt.utility.chance(prob=1.1)
+
+
+def test_resize_size():
+    """Test output shape when resizing tensors in 1D, 2D, and 3D."""
+    batch = 1
+    channel = 2
+    size_old = (8, 4, 7)
+    size_new = (5, 6, 8)
+
+    for dim in (1, 2, 3):
+        inp = torch.ones(batch, channel, *size_old[:dim])
+
+        # Expect output of requested shape.
+        out = kt.utility.resize(inp, size_new[:dim])
+        assert out.shape == (batch, channel, *size_new[:dim])
+
+        # Expect expansion of scalar sizes.
+        out = kt.utility.resize(inp, size_new[0])
+        assert out.shape == (batch, channel, *[size_new[0]] * dim)
+
+
+def test_resize_fill():
+    """Test resizing fill value."""
+    x = torch.ones(1, 1, 5)
+    size = x.shape[-1] + 1
+
+    # Default fill value should be zero.
+    out = kt.utility.resize(x, size=size)
+    assert out[..., -1] == 0
+
+    # Test setting specific fill value.
+    fill = -3
+    out = kt.utility.resize(x, size=size, fill=fill)
+    assert out[..., -1] == fill
+
+
+def test_resize_symmetry():
+    """Test symmetry of resize operation."""
+    x = [1, 2, 3, 4]
+    inp = torch.tensor(x).unsqueeze(0).unsqueeze(0)
+
+    # Expect even distribution of increase in size.
+    out = kt.utility.resize(inp, size=6).squeeze()
+    assert out.tolist() == [0, *x, 0]
+
+    # If increase is odd, expect one more element at upper end.
+    out = kt.utility.resize(inp, size=7).squeeze()
+    assert out.tolist() == [0, *x, 0, 0]
+
+    # Expect symmetric reduction in size.
+    out = kt.utility.resize(inp, size=2).squeeze()
+    assert out.tolist() == x[1:-1]
+
+    # If decrease is odd, expect one fewer element at upper end.
+    out = kt.utility.resize(inp, size=3).squeeze()
+    assert out.tolist() == x[:-1]
