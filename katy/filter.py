d6e34b57168440237af3169748ef0a6046514195
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2025-03-02 13:54:38 -0500

    Add binary morphology.

--- katy/filter.py
+++ katy/filter.py
@@ -75,3 +75,225 @@ def blur(x, fwhm, dim=None):
         x = x.view_as(tmp).transpose(i, -1)
 
     return x
+
+
+def dilate(x, n=1, *, dim=None):
+    """Perform binary morphological dilation in 1D, 2D, or 3D.
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Input tensor. We consider non-zero voxels foreground.
+    n : int, optional
+        Repeat the operation `n` times.
+    dim : int or sequence of int, optional
+        Operate along 1 to 3 dimensions. None means all.
+
+    Returns
+    -------
+    torch.Tensor
+        Dilated tensor.
+
+    """
+    # Input.
+    dtype = x.dtype
+    x = torch.as_tensor(x, dtype=torch.get_default_dtype())
+
+    # Dimensions.
+    if dim is None:
+        dim = torch.arange(x.ndim)
+    dim = torch.as_tensor(dim).ravel().tolist()
+    ndim = len(dim)
+
+    # Kernel: "4-neighborhood" in 2D.
+    kern = torch.zeros([3] * ndim)
+    for i in range(ndim):
+        kern[(*[1] * i, slice(None), *[1] * (ndim - i - 1))] = 1
+    kern = kern.unsqueeze(0).unsqueeze(0).to(x.device)
+
+    # Reshaping to: batch, 1 channel, working space.
+    end = tuple(range(-ndim, 0))
+    x = x.movedim(dim, end)
+    y = x.reshape(-1, 1, *x.shape[-ndim:])
+
+    f = getattr(torch.nn.functional, f'conv{ndim}d')
+    for _ in range(n):
+        y = f(y, kern, padding='same')
+
+    # Restore shape and threshold.
+    return y.view_as(x).movedim(end, dim).gt(0).to(dtype)
+
+
+def erode(x, n=1, *, dim=None):
+    """Perform binary morphological erosion in 1D, 2D, or 3D.
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Input tensor. We consider non-zero voxels foreground.
+    n : int, optional
+        Repeat the operation `n` times.
+    dim : int or sequence of int, optional
+        Operate along 1 to 3 dimensions. None means all.
+
+    Returns
+    -------
+    torch.Tensor
+        Eroded tensor.
+
+    """
+    # Dilate inverted input. For this duality to hold at the borders, we need
+    # to pad with ones, so we pad with zeros before the inversion.
+    dtype = x.dtype
+    x = torch.as_tensor(x)
+    x = torch.nn.functional.pad(x, pad=[1] * 2 * x.ndim).eq(0)
+    x = dilate(x, n, dim=dim).logical_not().to(dtype)
+    return x[(slice(1, -1),) * x.ndim]
+
+
+def close(x, n=1, *, dim=None):
+    """Perform binary morphological closing in 1D, 2D, or 3D.
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Input tensor. We consider non-zero voxels foreground.
+    n : int, optional
+        Dilate `n` times, then erode `n` times.
+    dim : int or sequence of int, optional
+        Operate along 1 to 3 dimensions. None means all.
+
+    Returns
+    -------
+    torch.Tensor
+        Closed tensor.
+
+    """
+    return erode(dilate(x, n, dim=dim), n, dim=dim)
+
+
+def open(x, n=1, *, dim=None):
+    """Perform binary morphological opening in 1D, 2D, or 3D.
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Input tensor. We consider non-zero voxels foreground.
+    n : int, optional
+        Erode `n` times, then dilate `n` times.
+    dim : int or sequence of int, optional
+        Operate along 1 to 3 dimensions. None means all.
+
+    Returns
+    -------
+    torch.Tensor
+        Opened tensor.
+
+    """
+    return dilate(erode(x, n, dim=dim), n, dim=dim)
+
+
+def fill_holes(x, *, dim=None):
+    """Perform binary morphological hole filling in 1D, 2D, or 3D.
+
+    The algorithm floods the background from the image border. We consider
+    holes that touch the border background and will not fill these.
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Input tensor. We consider non-zero voxels foreground.
+    dim : int or sequence of int, optional
+        Operate along 1 to 3 dimensions. None means all.
+
+    Returns
+    -------
+    torch.Tensor
+        Hole-filled tensor.
+
+    """
+    # Inverted input.
+    dtype = x.dtype
+    x = torch.as_tensor(x).gt(0)
+    inv = x.logical_not()
+
+    # Dimensions.
+    if dim is None:
+        dim = torch.arange(x.ndim)
+    dim = torch.as_tensor(dim).ravel()
+
+    # Seeds: background voxels touching the boundary.
+    mask = torch.zeros_like(x)
+    for i in dim:
+        for end in (0, -1):
+            ind = [slice(None)] * x.ndim
+            ind[i] = end
+            mask[ind] = inv[ind]
+
+    # Flood background from seeds until no more change.
+    prev = torch.zeros_like(mask)
+    while not mask.equal(prev):
+        prev = mask
+        mask = dilate(mask, dim=dim).logical_and(inv)
+
+    return mask.logical_not().to(dtype)
+
+
+def label(x):
+    """Label N-dimensional binary connected foreground components.
+
+    The algorithm uses (minimum) label propagation. We initialize foreground
+    pixels to unique index labels and iteratively replace each pixel with the
+    lowest label within its neighborhood, until convergence.
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Input tensor. We consider non-zero voxels foreground.
+
+    Returns
+    -------
+    torch.Tensor
+        Labeled components.
+    torch.Tensor
+        Unique labels.
+
+    """
+    # Padding prevents components from wrapping around the border.
+    x = torch.as_tensor(x)
+    x = torch.nn.functional.pad(x, pad=[1] * x.ndim * 2)
+
+    # Initial index labels. Floating point allows infinite background.
+    labels = torch.arange(x.numel(), dtype=torch.float32, device=x.device)
+    labels = labels.view_as(x)
+    bg = x.eq(0)
+    labels[bg] = torch.inf
+
+    # Kernel: 4-neighborhood in 2D.
+    steps = torch.eye(x.ndim, dtype=torch.int)
+    steps = torch.cat((-steps, steps))
+    steps = (*(s.tolist() for s in steps), [0] * x.ndim)
+
+    # Minimum label propagation. Restore background after each pass.
+    prev = torch.zeros_like(labels)
+    dims = tuple(range(x.ndim))
+    while not labels.equal(prev):
+        prev = labels
+        neigh = [torch.roll(labels, shifts=s, dims=dims) for s in steps]
+        labels, _ = torch.stack(neigh).min(dim=0)
+        labels[bg] = torch.inf
+
+    # Remove padding, order by size.
+    labels = labels[(slice(1, -1),) * labels.ndim]
+    uniq, size = labels.unique(return_counts=True)
+    if uniq.numel() and uniq[-1] == torch.inf:
+        uniq, size = uniq[:-1], size[:-1]
+    uniq = uniq[size.argsort(descending=True)]
+
+    # Map to indices. Initialize last label to 0 in case of background only.
+    new = 0
+    out = torch.zeros_like(labels, dtype=torch.long)
+    for new, old in enumerate(uniq, start=1):
+        out[labels == old] = new
+
+    return out, torch.arange(1, 1 + new)
--- tests/test_filter.py
+++ tests/test_filter.py
@@ -2,6 +2,7 @@
 
 
 import torch
+import pytest
 import katy as kt
 
 
@@ -39,3 +40,265 @@ def test_blur_default_dtype():
     """Test the default output data type."""
     x = torch.randint(10, size=(5,))
     assert kt.filter.blur(x, fwhm=2).dtype == torch.get_default_dtype()
+
+
+def test_dilate_trivial():
+    """Test dilation output data type and values."""
+    for dtype in (torch.int32, torch.float32):
+        x = torch.tensor((0.0, 0.0, 2.2, 3.3), dtype=dtype)
+        y = kt.filter.dilate(x, dim=0)
+
+        assert y.dtype == x.dtype
+        assert torch.isin(y, torch.tensor((0, 1))).all()
+
+
+def test_dilate_iterations():
+    """Test iterative dilation, in 1D."""
+    x = torch.tensor((0, 0, 1, 0, 0))
+    assert kt.filter.dilate(x, n=0).equal(x)
+    assert kt.filter.dilate(x, n=1).equal(torch.tensor((0, 1, 1, 1, 0)))
+    assert kt.filter.dilate(x, n=2).equal(torch.tensor((1, 1, 1, 1, 1)))
+
+
+def test_dilate_dimensions():
+    """Test dilating specific dimensions, in 2D."""
+    # Expected input and output.
+    a = torch.tensor((
+        (0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0),
+        (0, 0, 1, 0, 0),
+        (0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0),
+    ))
+    b = torch.tensor((
+        (0, 0, 0, 0, 0),
+        (0, 0, 1, 0, 0),
+        (0, 1, 1, 1, 0),
+        (0, 0, 1, 0, 0),
+        (0, 0, 0, 0, 0),
+    ))
+
+    # Copies along dimension 0.
+    inp = torch.zeros(3, 2, *a.shape)
+    inp[:, 0] = a
+    out = kt.filter.dilate(inp, dim=(3, 2))
+
+    # Expect only zeros at end of dimension 1.
+    assert out[:, 1].eq(0).all()
+
+    # Expect identical results in dimension 0.
+    for batch in out[:, 0]:
+        assert batch.equal(b)
+
+
+def test_erode_trivial():
+    """Test erosion output data type and values."""
+    for dtype in (torch.long, torch.float16):
+        x = torch.tensor((0.0, 1.1, 2.2, 3.3), dtype=dtype)
+        y = kt.filter.erode(x, dim=-1)
+
+        assert y.dtype == x.dtype
+        assert torch.isin(y, torch.tensor((0, 1))).all()
+
+
+def test_erode_iterations():
+    """Test iterative erosion, in 1D. See Scipy examples."""
+    x = torch.tensor((0, 1, 1, 1, 1, 1))
+    assert kt.filter.erode(x, n=0).equal(x)
+    assert kt.filter.erode(x, n=1).equal(torch.tensor((0, 0, 1, 1, 1, 0)))
+    assert kt.filter.erode(x, n=2).equal(torch.tensor((0, 0, 0, 1, 0, 0)))
+
+
+def test_erode_dimensions():
+    """Test eroding specific dimensions, in 2D. See Scipy examples."""
+    # Expected input and output.
+    a = torch.tensor((
+        (0, 0, 0, 0),
+        (0, 1, 1, 1),
+        (0, 1, 1, 1),
+        (0, 1, 1, 1),
+    ))
+    b = torch.tensor((
+        (0, 0, 0, 0),
+        (0, 0, 0, 0),
+        (0, 0, 1, 0),
+        (0, 0, 0, 0),
+    ))
+
+    # Copies along dimension 2.
+    inp = torch.zeros(*a.shape, 3, 2)
+    inp[..., 0, 0] = a
+    inp[..., 1, 0] = a
+    inp[..., 2, 0] = a
+    out = kt.filter.erode(inp, dim=(0, 1))
+
+    # Expect only zeros at end of dimension 3.
+    assert out[..., :, 1].eq(0).all()
+
+    # Expect identical results in dimension 2.
+    for i in range(out.shape[2]):
+        assert out[..., i, 0].equal(b)
+
+
+def test_close_1d():
+    """Test closing, in 1D. See Scipy examples."""
+    a = torch.tensor((0, 0, 1, 1, 0, 1))
+    b = torch.tensor((0, 0, 1, 1, 1, 0))
+
+    assert kt.filter.close(a, n=0).equal(a)
+    assert kt.filter.close(a, n=1).equal(b)
+
+
+def test_close_2d():
+    """Test closing, in 2D. See Scipy examples."""
+    a = torch.tensor((
+        (0, 0, 0, 0, 0),
+        (0, 1, 1, 1, 0),
+        (0, 1, 0, 1, 0),
+        (0, 1, 1, 1, 0),
+        (0, 0, 0, 0, 0),
+    ))
+    b = torch.tensor((
+        (0, 0, 0, 0, 0),
+        (0, 1, 1, 1, 0),
+        (0, 1, 1, 1, 0),
+        (0, 1, 1, 1, 0),
+        (0, 0, 0, 0, 0),
+    ))
+    assert kt.filter.close(a).equal(b)
+
+
+def test_open_1d():
+    """Test opening, in 1D. See Scipy examples."""
+    a = torch.tensor((0, 1, 1, 1, 0, 1))
+    b = torch.tensor((0, 1, 1, 1, 0, 0))
+    assert kt.filter.open(a, n=0).equal(a)
+    assert kt.filter.open(a, n=1).equal(b)
+
+
+def test_open_2d():
+    """Test opening, in 2D. See Scipy examples."""
+    a = torch.tensor((
+        (1, 0, 0, 0, 1),
+        (0, 1, 1, 1, 0),
+        (0, 1, 1, 1, 0),
+        (0, 1, 1, 1, 0),
+        (1, 0, 0, 0, 1),
+    ))
+    b = torch.tensor((
+        (0, 0, 0, 0, 0),
+        (0, 0, 1, 0, 0),
+        (0, 1, 1, 1, 0),
+        (0, 0, 1, 0, 0),
+        (0, 0, 0, 0, 0),
+    ))
+    assert kt.filter.open(a).equal(b)
+
+
+def test_fill_holes_trivial():
+    """Test hole-filling output data type and values, in 1D."""
+    for dtype in (torch.long, torch.float16):
+        x = torch.tensor((0.0, 1.1, 0.0, 3.3), dtype=dtype)
+        y = kt.filter.fill_holes(x, dim=0)
+
+        assert y.dtype == x.dtype
+        assert y.equal(torch.tensor((0, 1, 1, 1)))
+
+
+def test_fill_holes_dimensions():
+    """Test hole-filling along specific dimensions, in 2D."""
+    # Expected input and output.
+    a = torch.tensor((
+        (0, 0, 1, 1, 1),
+        (0, 0, 1, 1, 0),
+        (0, 1, 0, 0, 1),
+        (0, 1, 1, 1, 1),
+        (0, 1, 1, 0, 0),
+    ))
+    b = torch.tensor((
+        (0, 0, 1, 1, 1),
+        (0, 0, 1, 1, 0),
+        (0, 1, 1, 1, 1),
+        (0, 1, 1, 1, 1),
+        (0, 1, 1, 0, 0),
+    ))
+
+    # Copies along dimension 0.
+    inp = torch.zeros(3, 2, *a.shape)
+    inp[:, 0] = a
+    out = kt.filter.fill_holes(inp, dim=(-2, -1))
+
+    # Expect only zeros at end of dimension 1.
+    assert out[:, 1].eq(0).all()
+
+    # Expect identical results in dimension 0.
+    for batch in out[:, 0]:
+        assert batch.equal(b)
+
+
+def test_fill_holes_unchanged():
+    """Test hole-filling without holes has no effect, in 3D."""
+    for f in (torch.ones, torch.zeros):
+        inp = f(4, 4, 4)
+        out = kt.filter.fill_holes(inp)
+        assert out.equal(inp)
+
+    # Expect a copy.
+    assert out is not inp
+
+
+def test_fill_holes_illegal():
+    """Test if hole-filling a 4D tensor raises an error."""
+    x = torch.zeros(2, 2, 2, 2)
+
+    with pytest.raises(AttributeError):
+        kt.filter.fill_holes(x)
+
+
+def test_label_flat():
+    """Test labeling flat tensors, including types."""
+    x = torch.tensor((0, 0, 0, 0))
+    labels, values = kt.filter.label(x)
+    assert labels.dtype == torch.long
+    assert labels.eq(0).all()
+    assert values.numel() == 0
+
+    x = torch.tensor((2.2, 2.2, 2.2))
+    labels, values = kt.filter.label(x)
+    assert labels.dtype == torch.long
+    assert labels.eq(1).all()
+    assert values.equal(torch.tensor([1]))
+
+
+def test_label_1d():
+    """Test labeling a 1D tensor."""
+    x = torch.tensor((0, 1, 1, 0, 1, 2, 3, 0, 9))
+    labels, values = kt.filter.label(x)
+
+    # Expect islands labeled by decreasing size.
+    assert labels.equal(torch.tensor((0, 2, 2, 0, 1, 1, 1, 0, 3)))
+    assert values.equal(torch.tensor((1, 2, 3)))
+
+
+def test_label_2d():
+    """Test labeling a 2D tensor."""
+    # Expected input and output.
+    a = torch.tensor((
+        (1, 4, 0, 0, 5),
+        (2, 3, 0, 0, 5),
+        (0, 0, 9, 0, 5),
+        (0, 0, 0, 0, 5),
+        (5, 5, 0, 0, 5),
+    ))
+    b = torch.tensor((
+        (2, 2, 0, 0, 1),
+        (2, 2, 0, 0, 1),
+        (0, 0, 4, 0, 1),
+        (0, 0, 0, 0, 1),
+        (3, 3, 0, 0, 1),
+    ))
+
+    # Expect islands labeled by decreasing size.
+    labels, values = kt.filter.label(a)
+    assert labels.equal(b)
+    assert values.equal(torch.tensor((1, 2, 3, 4)))
